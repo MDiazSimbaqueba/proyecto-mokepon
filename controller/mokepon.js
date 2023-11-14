c914501172f5ffdc6367a352c3c82fb4bfc2ad49
Miguel Diaz <miguelzaiid@gmail.com>
2023-11-13 21:20:47 -0500

    Se realiza la logica para seleccionar la mascota

--- controller/mokepon.js
+++ controller/mokepon.js
@@ -1,1 +1,26 @@
+// window.addEventListener('load', () => {
+let botonMascotaJugador = document.getElementById('boton-mascota')
+let mascotaHipodoge = document.getElementById('hipodoge')
+let mascotaCapipepo = document.getElementById('capipepo')
+let mascotaRatigueya = document.getElementById('ratigueya')
+let mascotaLangostelvis = document.getElementById('langostelvis')
+let mascotaTucapalma = document.getElementById('tucapalma')
+let mascotaPydon = document.getElementById('pydon')
 
+    botonMascotaJugador.addEventListener('click', ()=>{
+        if(mascotaHipodoge.checked){
+            alert("Seleccionaste a Hipodoge")
+        }else if(mascotaCapipepo.checked){
+            alert("Seleccionaste a Capipepo")
+        }else if(mascotaRatigueya.checked){
+            alert("Seleccionaste a Ratigueya")
+        }else if(mascotaLangostelvis.checked){
+            alert("Seleccionaste a Langostelvis")
+        }else if(mascotaTucapalma.checked){
+            alert("Seleccionaste a Tucapalma")
+        }else if(mascotaPydon.checked){
+            alert("Seleccionaste a Pydon")
+        }else{
+            alert("Selecciona una mascota")
+        }
+    })
